e042e845283d1bb70f99aa53c694100558626656
Federico Cerisola <federico@cerisola.net>
2017-05-13 23:37:32 -0300

    Implemented fill lattice function

--- src/lattice.c
+++ src/lattice.c
@@ -1,8 +1,18 @@
 #include "lattice.h"
+#include <stdlib.h>
 
 int fill_lattice(int * lattice, int n, float p)
 {
-  return 0;
+    double q;
+    for (int i = 0; i < n*n; i++) {
+        q = ((double)rand())/RAND_MAX;
+        if (q < p) {
+            lattice[i] = 1;
+        } else {
+            lattice[i] = -1;
+        }
+    }
+    return 0;
 }
 
 int print_lattice(int * lattice, int n)
