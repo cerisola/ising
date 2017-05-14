5dcdfe2dbf88ee79ea4747af98f4f35f2e6343c0
Federico Cerisola <federico@cerisola.net>
2017-05-13 23:50:29 -0300

    Add calculations of average energy and magnetization to main program

--- src/ising.c
+++ src/ising.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 #include "metropolis.h"
 #include "lattice.h"
+#include "thermo.h"
 
 int main(int argc, char ** argv)
 {
@@ -14,10 +16,18 @@ int main(int argc, char ** argv)
     srand(time(NULL));
 
     fill_lattice(lattice, n, prob);
+    double Mavg = 0;
+    double Eavg = 0;
     for (int i = 0; i < niter; i++) {
         metropolis(lattice, n, T);
+        Mavg += magnetization(lattice, n);
+        Eavg += energy(lattice, n);
     }
+    Mavg /= niter;
+    Eavg /= niter;
+
     print_lattice(lattice, n);
+    printf("<M> = %f \t\t <E> = %f\n", Mavg, Eavg);
 
     return 0;
 }
