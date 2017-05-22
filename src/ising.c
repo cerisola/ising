bcf3bf03999b78db33e3928fe6bacc67e518ff96
Federico Cerisola <federico@cerisola.net>
2017-05-22 20:54:14 -0300

    Dynamically set number of iterations based on lattice size
    This change is needed due to the change in the way that the metropolis function
    works as introduced in c892314c10cb10a81523356f2958253694f568a8.

--- src/ising.c
+++ src/ising.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 
 #include "metropolis.h"
@@ -12,7 +13,9 @@ int main(int argc, char ** argv)
     int * lattice = malloc(n * n * sizeof(int));
     double prob = 0.5;
     double T = 2.0;
-    int niter = 2000;
+    int nsamples = 2000;
+    int nsep = 20 * n*n;
+    int niter = (nsamples + 1) * nsep;
     srand(time(NULL));
 
     fill_lattice(lattice, n, prob);
@@ -20,11 +23,13 @@ int main(int argc, char ** argv)
     double Eavg = 0;
     for (int i = 0; i < niter; i++) {
         metropolis(lattice, n, T);
-        Mavg += magnetization(lattice, n);
-        Eavg += energy(lattice, n);
+        if (i > 0 && i % nsep == 0) {
+            Mavg += fabs(magnetization(lattice, n));
+            Eavg += energy(lattice, n);
+        }
     }
-    Mavg /= niter;
-    Eavg /= niter;
+    Mavg /= nsamples;
+    Eavg /= nsamples;
 
     print_lattice(lattice, n);
     printf("<M> = %f \t\t <E> = %f\n", Mavg, Eavg);
