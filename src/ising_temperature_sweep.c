cerisola/ising

commit 2c252fea46496b127317b79c04e1dce3aa9dd54b

Changed algorithm used to calculate variance to a more stable one
Welford's algorithm [1][2] for computing the variance is far more stable
numerically than the naive implementation previously used. This produces much
better results, especially near the critical point.

[1]: B. P. Welford (1962). "Note on a method for calculating corrected sums of
squares and products". Technometrics 4(3):419–420.
[2]: Donald E. Knuth (1998). The Art of Computer Programming, volume 2:
Seminumerical Algorithms, 3rd edn., p. 232. Boston: Addison-Wesley.

src/ising_temperature_sweep.c
@@ -80,16 +80,14 @@ int main(int argc, char ** argv)
         for (int j = 0; j < niter; j++) {
             metropolis(lattice, n, &parameters, &quantities);
             if (j > 0 && j % nsep == 0) {
-                Mavg[i] += quantities.M;
-                Eavg[i] += quantities.E;
-                Mvar[i] += quantities.M * quantities.M;
-                Evar[i] += quantities.E * quantities.E;
+                update_online_mean_variance(quantities.M, j / nsep, Mavg + i, Mvar + i);
+                update_online_mean_variance(quantities.E, j / nsep, Eavg + i, Evar + i);
             }
         }
-        Mvar[i] = (Mvar[i] - (Mavg[i] * Mavg[i])/nsamples)/(nsamples - 1);
-        Evar[i] = (Evar[i] - (Eavg[i] * Eavg[i])/nsamples)/(nsamples - 1);
         Mavg[i] /= nsamples;
         Eavg[i] /= nsamples;
+        Mvar[i] /= nsamples - 1;
+        Evar[i] /= nsamples - 1;
 
         printf("Finished T_%d out of %d\n", j+1, npoints);
     }

src/math_extra.c
@@ -23,3 +23,14 @@ double * create_linear_grid(const double xmin, const double xmax,
 
     return grid;
 }
+
+int update_online_mean_variance(double new_value, int n, double * mean, double * variance)
+{
+    double delta = new_value - (*mean);
+    (*mean) += delta / n;
+
+    double delta2 = new_value - (*mean);
+    (*variance) += delta * delta2;
+
+    return 0;
+}

src/math_extra.h
@@ -32,4 +32,25 @@
 double * create_linear_grid(const double xmin, const double xmax,
                             const int npoints, const int round_digits);
 
+
+/*! Update the mean and variance for an online sampling process.
+
+    This function updates the values of the sample mean and variance from an
+    online sampling process.
+
+    @param new_value the new value to be used to update statistics.
+    @param n the number of the new sample.
+    @param mean where the mean is stored. On function call it contains the old
+        estimate of the mean and therefore on the first call should be initialized
+        to 0. On function return in contains the updated value. The returned value is
+        unnormalized and should be divided by the total number of observations at the end
+        of the sampling process.
+    @param variance where the variance is stored. On function call it contains the old
+        estimate of the variance and therefore on the first call should be initialized
+        to 0. On function return in contains the updated value. The returned value is
+        unnormalized and should be divided by the total number of observations at the end
+        of the sampling process.
+*/
+int update_online_mean_variance(double new_value, int n, double * mean, double * variance);
+
 #endif /* MATH_EXTRA_H */

commit e81e6ad05bebea3ee6f43835764ff75cb10743a7

Fixed bug in calculation of average values
This fixes a bug introduced in 2c252fea46496b127317b79c04e1dce3aa9dd54b when
calculating averages of the thermodynamic quantities.

src/ising_temperature_sweep.c
@@ -85,8 +85,6 @@ int main(int argc, char ** argv)
                 update_online_mean_variance(quantities.E, j / nsep, Eavg + i, Evar + i);
             }
         }
-        Mavg[i] /= nsamples;
-        Eavg[i] /= nsamples;
         Mvar[i] /= nsamples - 1;
         Evar[i] /= nsamples - 1;
 